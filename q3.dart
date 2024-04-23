0432fc45657822a1a738647c58dc9c48fbd0e36d
김현지 <s2208@e-mirim.hs.kr>
2024-04-23 22:04:01 +0900

    Create q3.dart

---
+++ q3.dart
@@ -0,0 +1,10 @@
+void main(){
+  //문제 3
+  //사각형의 넓이를 구하는 함수를 작성하고 결과 출력
+  var area = calculateArea(4, 3);
+  print(area);
+}
+
+int calculateArea(int width, int height) {
+  return width * height;
+}
