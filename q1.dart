0dcd7628002cc9e78091afeb62d8c55ec9205258
김현지 <s2208@e-mirim.hs.kr>
2024-04-23 22:02:46 +0900

    Create q1.dart

---
+++ q1.dart
@@ -0,0 +1,8 @@
+void main(){
+  //문제 1 : 변수 선언 및 데이터 타입
+  int number = 0; //정수형 데이터
+  String message = 'hi'; //문자열 데이터
+  double decimal = 1.0; //실수형 데이터
+  bool isAdult = true; //논리 자료형 (true or false)
+  List<String> names = ["Kim", "Lee", "Park"]; //리스트 형식
+}
